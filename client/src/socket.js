b791246ada456f47b174d618917d5f113c124512
Zorawar Singh Mahal <118074455+zorawarMahal@users.noreply.github.com>
2025-06-21 23:42:46 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,9 +1,5 @@
 import {io} from 'socket.io-client';
 
-<<<<<<< HEAD
-const socket = io("http://localhost:3001");
-=======
 const socket = io("https://meme-marketplace-dhpl.onrender.com");
->>>>>>> 7c01c56d7976174cfc6b3734ada0db961de111f8
 
 export default socket;
